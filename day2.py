e906da1c4fba54a38a2afd9038b6a4c1f114ef58
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-07 23:35:36 +0600

    another alphabet game

--- day2.py
+++ day2.py
@@ -1,25 +1,49 @@
-# Suppose you are given the below string
+# # Suppose you are given the below string
 
-# str = “””Email_Address,Nickname,Group_Status,Join_Year
+# # str = “””Email_Address,Nickname,Group_Status,Join_Year
+# # aa@aaa.com,aa,Owner,2014
+# # bb@bbb.com,bb,Member,2015
+# # cc@ccc.com,cc,Member,2017
+# # dd@ddd.com,dd,Member,2016
+# # ee@eee.com,ee,Member,2020
+# # “””
+
+# # In order to extract only the domain names from the email addresses from the above string (for eg. “aaa”, “bbb”..) you write the following code:
+
+# import re
+
+# str_data = """Email_Address,Nickname,Group_Status,Join_Year
 # aa@aaa.com,aa,Owner,2014
 # bb@bbb.com,bb,Member,2015
 # cc@ccc.com,cc,Member,2017
 # dd@ddd.com,dd,Member,2016
 # ee@eee.com,ee,Member,2020
-# “””
+# """
 
-# In order to extract only the domain names from the email addresses from the above string (for eg. “aaa”, “bbb”..) you write the following code:
+# domains = re.findall(r'@([a-zA-Z0-9]+)\.', str_data)
 
-import re
+# print(domains)
 
-str_data = """Email_Address,Nickname,Group_Status,Join_Year
-aa@aaa.com,aa,Owner,2014
-bb@bbb.com,bb,Member,2015
-cc@ccc.com,cc,Member,2017
-dd@ddd.com,dd,Member,2016
-ee@eee.com,ee,Member,2020
-"""
+# Your friend has a hypothesis – “All those people who have names ending with the sound of “y” (Eg: Hollie) are intelligent people.” Please note: The name should end with the sound of ‘y’ but not end with alphabet ‘y’.
 
-domains = re.findall(r'@([a-zA-Z0-9]+)\.', str_data)
+# Now you being a data freak, challenge the hypothesis by scraping data from your college’s website. Here’s data you have collected.
 
-print(domains)
+# Name	Marks
+# Andy	0
+# Mandi	10
+# Sandy	20
+# Hollie	18
+# Molly	19
+# Dollie	15
+
+# You want to make a list of all people who fall in this category. You write following code do to the same:
+
+names = ["Andy", "Mandi", "Sandy", "Hollie", "Molly", "Dollie"]
+
+result = []
+
+for name in names:
+    if name.lower().endswith("ie"):
+        result.append(name)
+
+print(result)
